cb2adac8c8b61965bb0499472f527880bb034da4
MDornacher <manuel.dornacher@gmail.com>
2020-05-10 18:14:58 +0200

    initial commit

---
+++ dashboard.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import plotly.express as px
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+df = pd.read_csv('speedtests.csv')
+fig = px.line(df, x='Timestamp', y='Download')
+
+app = dash.Dash()
+app.layout = html.Div([
+    dcc.Graph(figure=fig)
+])
+app.run_server(host='0.0.0.0')
