7f866f51549dbda9713cfd4441e40afe1719fcc5
MDornacher <manuel.dornacher@gmail.com>
2020-05-13 22:04:26 +0200

    add if name is main

--- dashboard.py
+++ dashboard.py
@@ -34,31 +34,33 @@ def timedelta_from_utc():
     dt_utc = datetime.utcnow()
     return dt_now - dt_utc
 
-# import data
-df_speedtest = pd.read_csv(os.path.join('example', 'speedtests.csv'))
 
-# convert timestamp
-df_speedtest['Timestamp'] = pd.to_datetime(df_speedtest['Timestamp'])
-df_speedtest['Timestamp'] = df_speedtest['Timestamp'] + timedelta_from_utc()
+if __name__ == "__main__":
+    # import data
+    df_speedtest = pd.read_csv(os.path.join('example', 'speedtests.csv'))
 
-# add groupable timestamp variations
-df_speedtest['Hour'] = [ts.hour for ts in df_speedtest['Timestamp']]
-df_speedtest['Weekday'] = [ts.weekday() for ts in df_speedtest['Timestamp']]
-df_speedtest['Week'] = [ts.week for ts in df_speedtest['Timestamp']]
-df_speedtest['Month'] = [ts.month for ts in df_speedtest['Timestamp']]
-df_speedtest['Year'] = [ts.year for ts in df_speedtest['Timestamp']]
+    # convert timestamp
+    df_speedtest['Timestamp'] = pd.to_datetime(df_speedtest['Timestamp'])
+    df_speedtest['Timestamp'] = df_speedtest['Timestamp'] + timedelta_from_utc()
 
-# prepare plots
-fig = fig_from_df_cols(df_speedtest, 'Timestamp', ['Download', 'Upload', 'Ping'])
-fig.update_layout(title_text='Timeseries of Speedtests',
-                  template='plotly_dark',
-                  height=600,
-                  width=800,
-                  )
+    # add groupable timestamp variations
+    df_speedtest['Hour'] = [ts.hour for ts in df_speedtest['Timestamp']]
+    df_speedtest['Weekday'] = [ts.weekday() for ts in df_speedtest['Timestamp']]
+    df_speedtest['Week'] = [ts.week for ts in df_speedtest['Timestamp']]
+    df_speedtest['Month'] = [ts.month for ts in df_speedtest['Timestamp']]
+    df_speedtest['Year'] = [ts.year for ts in df_speedtest['Timestamp']]
 
-# create dashboard
-app = dash.Dash()
-app.layout = html.Div([
-    dcc.Graph(figure=fig)
-])
-app.run_server(host='0.0.0.0')
+    # prepare plots
+    fig = fig_from_df_cols(df_speedtest, 'Timestamp', ['Download', 'Upload', 'Ping'])
+    fig.update_layout(title_text='Timeseries of Speedtests',
+                      template='plotly_dark',
+                      height=600,
+                      width=800,
+                      )
+
+    # create dashboard
+    app = dash.Dash()
+    app.layout = html.Div([
+        dcc.Graph(figure=fig)
+    ])
+    app.run_server(host='0.0.0.0')
