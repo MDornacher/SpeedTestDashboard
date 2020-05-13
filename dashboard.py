df3a2c86fb0b56f4b64988907a62b2005420dcaf
MDornacher <manuel.dornacher@gmail.com>
2020-05-13 10:27:32 +0200

    use subplot for download, upload and ping

--- dashboard.py
+++ dashboard.py
@@ -1,15 +1,39 @@
 import os
 
 import pandas as pd
-import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
 
-df = pd.read_csv(os.path.join('example', 'speedtests.csv'))
-fig = px.line(df, x='Timestamp', y='Download')
+def fig_from_df_cols(df, x_col, y_cols):
+    fig = make_subplots(rows=len(y_cols),
+                        cols=1,
+                        shared_xaxes=True
+                        )
+
+    for i, col_name in enumerate(y_cols, 1):
+        fig.add_trace(
+            go.Scatter(x=df[x_col],
+                       y=df[col_name],
+                       name=col_name,
+                       mode='lines+markers',
+                       ),
+            row=i, col=1)
+    return fig
+
+
+df_speedtest = pd.read_csv(os.path.join('example', 'speedtests.csv'))
+
+fig = fig_from_df_cols(df_speedtest, 'Timestamp', ['Download', 'Upload', 'Ping'])
+fig.update_layout(title_text='Timeseries of Speedtests',
+                  template='plotly_dark',
+                  )
+
 
 app = dash.Dash()
 app.layout = html.Div([
